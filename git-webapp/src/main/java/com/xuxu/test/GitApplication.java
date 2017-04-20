cf4a7a99a0718ae72120f050c6424e02c3fd1b9a
xuxu <xuxu@weidai.com>
2017-04-21 00:18:37 +0800

    first commit

---
+++ git-webapp/src/main/java/com/xuxu/test/GitApplication.java
@@ -0,0 +1,25 @@
+package com.xuxu.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+/**
+ * Created by xuxu on 2017/4/20.
+ */
+@SpringBootApplication
+@RestController
+public class GitApplication {
+    public static void  main(String[] args) {
+        SpringApplication.run(GitApplication.class, args);
+    }
+
+    @RequestMapping(value="/test")
+    public String test(){
+        System.out.println("ddd");
+        return "dwdw";
+    }
+
+}
